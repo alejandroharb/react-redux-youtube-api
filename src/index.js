67b827b0bf2b7d4cb49bdec1f83c254dda31124d
Alejandro Harb <alejandroharb@gmail.com>
2017-09-25 21:36:00 -0500

    working searchbar functionality, with video play feature

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import YTSearch from 'youtube-api-search';
 import reducers from './reducers';
-
+import _ from 'lodash';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
@@ -20,19 +20,27 @@ class App extends Component {
       selectedVideo: null
     };
 
-    YTSearch({key: API_KEY, term: 'surfboards' }, (videos) => {
+    this.videoSearch('surfboards');
+
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term }, (videos) => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
       }); //ES6 destructuring
     });
-
   }
 
   render(){
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
     return (
       <div>
-        <SearchBar />
+        <SearchBar
+          onSearchTermChange={videoSearch}
+        />
         <VideoDetail video={this.state.selectedVideo}/>
         <VideoList
           onVideoSelect={(selectedVideo) =>  this.setState({selectedVideo}) }
